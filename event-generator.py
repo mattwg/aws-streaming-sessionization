982b5bd0f9bc687022e38319da151ff1ac1c0ce4
BuildTools <unconfigured@null.spigotmc.org>
2019-02-18 14:02:31 -0800

    Added Kinesis Firehose code

--- event-generator.py
+++ event-generator.py
@@ -2,6 +2,7 @@ import random
 import datetime
 import operator
 from time import sleep
+import boto3
 
 def create_clickstream(sessions=10,
                         start_time=datetime.datetime(2019,1,1,0,0),
@@ -30,7 +31,7 @@ def create_clickstream(sessions=10,
 
     return sorted_clickstream
 
-def replay_clickstream(clickstream, speed=1.0):
+def replay_clickstream_local(clickstream, speed=1.0):
     # replays the clickstream starting now at the required speed
     now = datetime.datetime.now()
     current_time = datetime.datetime.strptime(clickstream[1]['hit_timestamp'],"%Y-%m-%dT%H:%M:%S")
@@ -43,8 +44,24 @@ def replay_clickstream(clickstream, speed=1.0):
         current_time = next_hit_time
         print(hit)
 
+def replay_clickstream_firehose(clickstream, region_name, delivery_stream, speed=1.0):
+    # replays the clickstream starting now at the required speed
+    firehose = boto3.client('firehose', region_name=region_name)
+    now = datetime.datetime.now()
+    current_time = datetime.datetime.strptime(clickstream[1]['hit_timestamp'],"%Y-%m-%dT%H:%M:%S")
+    # iterate through the ordered clickstream
+    for hit in clickstream:
+        next_hit_time = datetime.datetime.strptime(hit['hit_timestamp'],"%Y-%m-%dT%H:%M:%S")
+        seconds_to_next_hit = (next_hit_time-current_time).total_seconds()
+        if seconds_to_next_hit > 0:
+            sleep(seconds_to_next_hit/speed)
+        current_time = next_hit_time
+        firehose.put_record(DeliveryStreamName=delivery_stream,Record={'Data':str(hit)})
+
 cs = create_clickstream(sessions=5,
     start_time=datetime.datetime(2019,1,1,0,0),
     end_time=datetime.datetime(2019,1,1,0,2))
 
-replay_clickstream(cs,4)
+replay_clickstream_local(cs,4)
+
+replay_clickstream_firehose(cs,'us-east-1','clickstream-firehose',4)
